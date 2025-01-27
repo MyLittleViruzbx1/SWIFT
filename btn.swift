124c4e37d4c3341b87aa3e5dbd6f3f663a558278
MyLittleViruzbx1 <mylittleviruz@gmail.com>
2025-01-27 13:52:05 -0600

    luncher and icon

--- btn.swift
+++ btn.swift
@@ -38,3 +38,49 @@ struct ContentView2: View {
 #Preview {
     ContentView()
 }
+
+// canvas mensaje
+///
+//  ContentView.swift
+//  HelloSwift
+//
+//  Created by myz on 27/01/25.
+//
+//
+
+
+
+// import SwiftUI
+
+// struct ContentView: View {
+//     let btn = "Encriptar"
+//     @State private var show = false
+//     var body: some View {
+//         Button(action: {
+//             show = true
+//             print("Hola en consola")
+//         }) {
+//             Text(btn)
+//         }.alert(isPresented: $show, content: {
+//             Alert(title: Text("Encriptado Clave"), message:     Text("¡Listo!"), dismissButton: .default(Text("Aceptar")))
+//         })
+//     }
+// }
+
+// struct ContentView2: View {
+//     var body: some View {
+//         let nombre = "Alexander"
+//         VStack {
+//             Image(systemName: "globe")
+//                 .imageScale(.large)
+//                 .foregroundStyle(.tint)
+//             Text(nombre)
+//         }
+//         .padding()
+//     }
+// }
+
+
+// #Preview {
+//     ContentView()
+// }
